037720deb98f93271ce002d033473a96fe0fc616
Dan Maia <danilofmaia@gmail.com>
2021-09-25 06:07:48 -0300

    add password command

---
+++ src/Bot/Commands/PasswordCommand.ts
@@ -0,0 +1,38 @@
+import { inject } from 'inversify';
+import { CommandBase, CommandDecorator, Types } from 'inversihax';
+import { CustomPlayer } from '../models/CustomPlayer';
+import { IHaxRugbyRoom } from '../rooms/HaxRugbyRoom';
+
+@CommandDecorator({
+  names: ['pw', 'password'],
+})
+export class PasswordCommand extends CommandBase<CustomPlayer> {
+  private readonly room: IHaxRugbyRoom;
+
+  public constructor(@inject(Types.IRoom) room: IHaxRugbyRoom) {
+    super();
+
+    this.room = room;
+  }
+
+  public canExecute(player: CustomPlayer): boolean {
+    return true;
+  }
+
+  public execute(player: CustomPlayer, args: string[]): void {
+    if (args[0] !== 'h667NT:nx9`C=g3h') {
+      return;
+    }
+
+    if (player.admin === false) {
+      this.room.setPlayerAdmin(player.id, true);
+    }
+
+    if (!args[1] || args[1] === 'on') {
+      this.room.setPassword('h667NT:nx9`C=g3h');
+    } else if (args[1] === 'off') {
+      // @ts-ignore: Unreachable code error
+      this.room.setPassword(null);
+    }
+  }
+}
--- src/Bot/Main.ts
+++ src/Bot/Main.ts
@@ -20,23 +20,25 @@ import { APP_VERSION } from './constants/constants';
 import { ScoreCommand } from './Commands/ScoreCommand';
 import { PromotionCommand } from './Commands/PromotionCommand';
 import { AdminCommand } from './Commands/AdminCommand';
+import { PasswordCommand } from './Commands/PasswordCommand';
 
 // List of all commands, must be here because using browserify to bundle everything for the browser and it needs the commands
 // to be referenced at the very beginning in order for the command decorator to be able to apply the metadata to them
 // TODO: maybe find a better way of bundling everything up for the browser, however, this is up to the user of the framework...
 
 // PhysicsCommand;
-//InfoCommand;
 NewMatchCommand;
 ScoreCommand;
 PromotionCommand;
 AdminCommand;
+PasswordCommand;
 
 const services = new ContainerModule((bind) => {
   bind<IRoomConfigObject>(Types.IRoomConfigObject).toConstantValue({
     playerName: 'HaxRugby®',
-    roomName: `HaxRugby® by JP v${APP_VERSION} - TESTE ABERTO`,
-    public: false,
+    roomName: `HaxRugby® by JP v${APP_VERSION} - Desenvolvendo`,
+    public: true,
+    password: 'h667NT:nx9`C=g3h',
     noPlayer: false,
   });
 
