0be15bf15b1555c83f545a57544ffea955180b28
Dan Maia <danilofmaia@gmail.com>
2021-10-12 08:19:03 -0300

    experimenting

--- src/bot/constants/constants.ts
+++ src/bot/constants/constants.ts
@@ -22,7 +22,7 @@ export const AFTER_TRY_MAX_TICKS = 120;
 export const KICK_RATE_LIMIT = [10, 60, 3];
 
 export const BALL_TEAM_COLOR_TICKS = 60;
-export const AIR_KICK_TICKS = 90;
-export const AIR_KICK_BLOCK_TICKS = 75;
-export const SMALL_AIR_KICK_BOOST = 1.15;
+export const AIR_KICK_TICKS = 105;
+export const AIR_KICK_BLOCK_TICKS = 90;
+export const SMALL_AIR_KICK_BOOST = 1.3;
 export const NORMAL_AIR_KICK_BOOST = 1.3;
