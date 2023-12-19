5ebee1fbd9cd75f3e557a030fbe9aea569031e98
Dan Maia <danilofmaia@gmail.com>
2023-12-19 13:25:50 -0300

    config: change `!rr x4` map from Big to Normal feat: turn off `a` command

--- src/bot/singletons/matchConfigs.ts
+++ src/bot/singletons/matchConfigs.ts
@@ -5,7 +5,7 @@ export const matchConfigStrings: string[] = ['x1', 'x2', 'x3', 'x4', 'x5', 'x6']
 
 const x2: MatchConfig = new MatchConfig(2, 30, 20, MapSizeEnum.SMALL);
 const x3: MatchConfig = new MatchConfig(3, 30, 20, MapSizeEnum.NORMAL);
-const x4: MatchConfig = new MatchConfig(4, 30, 20, MapSizeEnum.BIG);
+const x4: MatchConfig = new MatchConfig(4, 30, 20, MapSizeEnum.NORMAL);
 const x5: MatchConfig = new MatchConfig(5, 30, 20, MapSizeEnum.BIG);
 const x6: MatchConfig = new MatchConfig(6, 30, 20, MapSizeEnum.BIG);
 
